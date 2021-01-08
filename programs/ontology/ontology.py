3666c2a4e2f6d8fdf6ab765624839c7749fbf129
cstirdivant <64609897+cstirdivant@users.noreply.github.com>
2021-01-08 08:28:29 -0700

    Make Ontology compatible with Google Digital Bldgs
    Updates include: changes to path.append to be compatible with Windows file paths and added check to default to 'GLOBAL' namespace if no namespace is explicitly stated.

--- programs/ontology/ontology.py
+++ programs/ontology/ontology.py
@@ -19,7 +19,7 @@ import json
 import yaml
 import sys
 
-sys.path.append('../')
+sys.path.append('..\\')
 
 # Proprietary Packages
 from pretty import PrettyPrint
@@ -309,6 +309,8 @@ class Types:
 				if len(i.split('/')) > 1:
 					tmp_namespace = i.split('/')[0]
 					tmp_i = i.split('/')[1]
+					if tmp_namespace == '':
+						tmp_namespace = 'GLOBAL'
 					_get_fields(tmp_namespace,tmp_i)
 
 				# Get the fields of a type in the global namespace
@@ -889,7 +891,7 @@ if __name__ == '__main__':
 
 
 
-	rdir = '../../Ontology/yaml/resources'
+	rdir = '..\\..\\ontology\\yaml\\resources'
 
 	ont = Ontology(rdir)
 
